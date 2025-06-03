f845e1cd1e4151e0c9f0dbde16eb14f7d86f72cd
alamin-7 <alamin188914@gmail.com>
2025-06-04 02:38:22 +0600

    chore: set up Spring Boot project with basic dependencies

---
+++ src/main/java/com/TaskRetrievalWithCaching/TaskRetrievalWithCachingApplication.java
@@ -0,0 +1,13 @@
+package com.TaskRetrievalWithCaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRetrievalWithCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRetrievalWithCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskRetrievalWithCaching/TaskRetrievalWithCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskRetrievalWithCaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRetrievalWithCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
